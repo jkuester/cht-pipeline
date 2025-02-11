62a33545dea71fb214f013c6de19dad1b1f51eda
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-02-11 13:34:20 -0600

    include form_indexes parameter

--- models/forms/jkuester.sql
+++ models/forms/jkuester.sql
@@ -1,7 +1,10 @@
+-- add any indexes specific to this form
+{%- set form_indexes = [] -%}
+
 -- add columns specific to this form
 {% set form_columns %}
   couchdb.doc->'fields' ->> 'hello' as hello,
 {% endset %}
 
 -- call the macro with the form name, the columns and indexes to create the actual model
-{{ cht_form_model('jkuester', form_columns) }}
+{{ cht_form_model('jkuester', form_columns, form_indexes) }}
