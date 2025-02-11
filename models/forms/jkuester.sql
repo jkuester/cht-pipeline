83764d8013b1f4aead70c9a7c128e11e0ee0d2da
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-02-11 13:51:22 -0600

    Go back to jkuester config, this time with actual table name

--- models/forms/jkuester.sql
+++ models/forms/jkuester.sql
@@ -1,17 +1,9 @@
 -- add any indexes specific to this form
-{%- set form_indexes = [{'columns': ['edd']}]-%}
+{%- set form_indexes = []-%}
 -- add columns specific to this form
 {% set form_columns %}
-  couchdb.doc->'fields' ->> 'risk_factors' AS risk_factors,
-
-  -- extract the ANC visit number
-  CASE
-    WHEN couchdb.doc->'fields' ->>'anc_visit_identifier' <> ''
-      THEN (couchdb.doc->'fields' ->>'anc_visit_identifier')::int
-    WHEN couchdb.doc->'fields' #>>'{group_repeat,anc_visit_repeat,anc_visit_identifier}' <> ''
-      THEN RIGHT(couchdb.doc->'fields' #>>'{group_repeat,anc_visit_repeat,anc_visit_identifier}'::text[], 1)::int
-    ELSE 0 -- if not included default to 0
-END AS anc_visit
+  couchdb.doc->'fields' ->> 'hello' AS hello,
+END AS jkuester
 {% endset %}
 
 -- call the macro with the form name, the columns and indexes to create the actual model
