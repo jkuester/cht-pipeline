4fcd2c011dc7a32c97adb04590dcef6c6c9737a9
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-02-11 13:49:52 -0600

    Switch to be more minimal

--- models/forms/jkuester.sql
+++ models/forms/jkuester.sql
@@ -1,16 +1,7 @@
 -- add any indexes specific to this form
-{%- set form_indexes = [
-  {'columns': ['edd']},
-  {'columns': ['danger_signs']},
-  {'columns': ['risk_factors']}]
--%}
+{%- set form_indexes = [{'columns': ['edd']}]-%}
 -- add columns specific to this form
 {% set form_columns %}
-  NULLIF(couchdb.doc->'fields'->>'lmp_date','')::date as lmp, -- CAST lmp string to date or NULL if empty
-  NULLIF(couchdb.doc->'fields' ->> 'edd','')::date as edd, -- CAST edd string to date or NULL if empty
-
-  couchdb.doc->'fields' ->> 'lmp_method' as lmp_method,
-  couchdb.doc->'fields' ->> 'danger_signs' AS danger_signs,
   couchdb.doc->'fields' ->> 'risk_factors' AS risk_factors,
 
   -- extract the ANC visit number
@@ -24,4 +15,4 @@ END AS anc_visit
 {% endset %}
 
 -- call the macro with the form name, the columns and indexes to create the actual model
-{{ cht_form_model('pregnancy', form_columns, form_indexes) }}
+{{ cht_form_model('jkuester', form_columns, form_indexes) }}
