2f52986cab7df25c18f9184ce35cd8491afbf152
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-02-11 13:28:09 -0600

    Add jkuester form model

---
+++ models/forms/jkuester.sql
@@ -0,0 +1,7 @@
+-- add columns specific to this form
+{% set form_columns %}
+  couchdb.doc->'fields' ->> 'hello' as hello,
+{% endset %}
+
+-- call the macro with the form name, the columns and indexes to create the actual model
+{{ cht_form_model('jkuester', form_columns) }}
