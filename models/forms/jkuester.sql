89581af2c52fec03f4d4bdda65df928994bcd864
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-02-11 13:46:50 -0600

    Try just using the example model

--- models/forms/jkuester.sql
+++ models/forms/jkuester.sql
@@ -1,10 +1,27 @@
 -- add any indexes specific to this form
-{%- set form_indexes = [] -%}
-
+{%- set form_indexes = [
+  {'columns': ['edd']},
+  {'columns': ['danger_signs']}]
+  {'columns': ['risk_factors']}]
+-%}
 -- add columns specific to this form
 {% set form_columns %}
-  couchdb.doc->'fields' ->> 'hello' as hello,
+  NULLIF(couchdb.doc->'fields'->>'lmp_date','')::date as lmp, -- CAST lmp string to date or NULL if empty
+  NULLIF(couchdb.doc->'fields' ->> 'edd','')::date as edd, -- CAST edd string to date or NULL if empty
+
+  couchdb.doc->'fields' ->> 'lmp_method' as lmp_method,
+  couchdb.doc->'fields' ->> 'danger_signs' AS danger_signs,
+  couchdb.doc->'fields' ->> 'risk_factors' AS risk_factors,
+
+  -- extract the ANC visit number
+  CASE
+    WHEN couchdb.doc->'fields' ->>'anc_visit_identifier' <> ''
+      THEN (couchdb.doc->'fields' ->>'anc_visit_identifier')::int
+    WHEN couchdb.doc->'fields' #>>'{group_repeat,anc_visit_repeat,anc_visit_identifier}' <> ''
+      THEN RIGHT(couchdb.doc->'fields' #>>'{group_repeat,anc_visit_repeat,anc_visit_identifier}'::text[], 1)::int
+    ELSE 0 -- if not included default to 0
+END AS anc_visit
 {% endset %}
 
 -- call the macro with the form name, the columns and indexes to create the actual model
-{{ cht_form_model('jkuester', form_columns, form_indexes) }}
+{{ cht_form_model('pregnancy', form_columns, form_indexes) }}
