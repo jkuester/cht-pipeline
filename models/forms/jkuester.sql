3628b5dfc696e9b268a9d0dbac0d6e6815c0e281
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-02-11 13:54:51 -0600

    Remove end statement since I think it goes with case.

--- models/forms/jkuester.sql
+++ models/forms/jkuester.sql
@@ -3,7 +3,6 @@
 -- add columns specific to this form
 {% set form_columns %}
   couchdb.doc->'fields' ->> 'hello' AS hello
-END AS jkuester
 {% endset %}
 
 -- call the macro with the form name, the columns and indexes to create the actual model
